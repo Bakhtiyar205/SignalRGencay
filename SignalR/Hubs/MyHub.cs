3ab15131855dcd40629c8ee35d0329e1fa422518
b.amrahov <b.amrahov@xalqsigorta.az>
2023-02-06 16:07:46 +0400

    ConnectionClientList

--- SignalR/Hubs/MyHub.cs
+++ SignalR/Hubs/MyHub.cs
@@ -1,13 +1,32 @@
 ﻿using Microsoft.AspNetCore.SignalR;
+using System.Runtime.CompilerServices;
 
 namespace SignalR.Hubs
 {
     public class MyHub :Hub
     {
+        static List<string> clients = new List<string>();
         public async Task SendMessageAsync(string message)
         {
            await Clients.All.SendAsync("receiveMessage",message);
             
+        }
+
+        //Connections are good for logging on SignalR
+        // ConnectionId: clients are named with unique id on Hub by System
+        //ConnectionId Aim: differ from other user
+        public override async Task OnConnectedAsync()
+        {
+            clients.Add(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userJoined", Context.ConnectionId);
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            clients.Remove(Context.ConnectionId);
+            await Clients.All.SendAsync("clients", clients);
+            await Clients.All.SendAsync("userLeaved", Context.ConnectionId);
         }
     }
 }
